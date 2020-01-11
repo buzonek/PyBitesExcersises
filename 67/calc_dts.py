8b9f9ce2168bd9a918983ec5525ccb1a0c208295
Mateusz Burcon <mateusz.burcon@ericsson.com>
2020-01-11 17:58:37 +0100

    submitting bite no 67

---
+++ 67/calc_dts.py
@@ -0,0 +1,18 @@
+from datetime import date, timedelta
+
+start_100days = date(2017, 3, 30)
+pybites_founded = date(2016, 12, 19)
+pycon_date = date(2018, 5, 8)
+
+
+def get_hundred_days_end_date():
+    """Return a string of yyyy-mm-dd"""
+    end_100days = start_100days + timedelta(days=100)
+    return end_100days.strftime('%Y-%m-%d')
+
+
+def get_days_between_pb_start_first_joint_pycon():
+    """Return the int number of days"""
+    diff = pycon_date - pybites_founded
+    return diff.days
+
