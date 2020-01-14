df6b2c1496d36d0dccd071343cfc08651884bde5
Mateusz Burcon <mateusz.burcon@ericsson.com>
2020-01-14 15:30:22 +0100

    uploading fix to the decorator

--- 120/validate.py
+++ 120/validate.py
@@ -4,9 +4,9 @@ from functools import wraps
 def int_args(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if any(type(x) == int for x in args):
+        if any(type(x) != int for x in args):
             raise TypeError
-        if all(x > 0 for x in args):
+        if any(x < 0 for x in args):
             raise ValueError
-        func(*args, **kwargs)
+        return func(*args, **kwargs)
     return wrapper
