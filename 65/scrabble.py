cfec096c53f1058df23cac29fd509b3e8ca7aa9a
buzonek <buzonekk@gmail.com>
2020-01-19 13:36:06 +0100

    submitting bite no 65

---
+++ 65/scrabble.py
@@ -0,0 +1,31 @@
+import itertools
+import os
+import urllib.request
+
+# PREWORK
+TMP = os.getenv("TMP", "/tmp")
+DICT = 'dictionary.txt'
+DICTIONARY = os.path.join(TMP, DICT)
+urllib.request.urlretrieve(
+    f'https://bites-data.s3.us-east-2.amazonaws.com/{DICT}',
+    DICTIONARY
+)
+
+with open(DICTIONARY) as f:
+    dictionary = set([word.strip().lower() for word in f.read().split()])
+
+
+def get_possible_dict_words(draw):
+    """Get all possible words from a draw (list of letters) which are
+       valid dictionary words. Use _get_permutations_draw and provided
+       dictionary"""
+    return [x for x in _get_permutations_draw(draw) if x in dictionary]
+
+
+def _get_permutations_draw(draw):
+    """Helper to get all permutations of a draw (list of letters), hint:
+       use itertools.permutations (order of letters matters)"""
+    all_permutations = list()
+    for i in range(1, len(draw) + 1):
+        all_permutations.extend([''.join(x).lower() for x in itertools.permutations(draw, i)])
+    return all_permutations
