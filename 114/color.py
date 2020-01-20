fc0f68280e0d6025c83272010c7f01eab53d44d3
buzonek <buzonekk@gmail.com>
2020-01-20 16:54:02 +0100

    fixes in rgb2h function

--- 114/color.py
+++ 114/color.py
@@ -27,22 +27,22 @@ class Color:
     @staticmethod
     def hex2rgb(hex_):
         """Class method that converts a hex value into an rgb one"""
-        if not hex_.startswith('#') and len(hex_) < 7:
+        if not hex_.startswith('#') or len(hex_) < 7:
             raise ValueError
-        rgb = hex_[1:3], hex_[3:5], hex_[5:7]
-        return (int(x, 16) for x in rgb)
+        rgb = (hex_[1:3], hex_[3:5], hex_[5:7])
+        return tuple(int(x, 16) for x in rgb)
 
     @staticmethod
     def rgb2hex(rgb):
         """Class method that converts an rgb value into a hex one"""
         if any(x for x in rgb if x not in range(0, 256)):
             raise ValueError
-        return f'#{"".join([hex(x).split("x")[1] for x in rgb])}'
+        return f'#{"".join(hex(x).split("x")[1].rjust(2, "0") for x in rgb)}'
 
     def __repr__(self):
         """Returns the repl of the object"""
-        return f'{self.__class__.__name__}({self.color})'
+        return f"{self.__class__.__name__}('{self.color}')"
 
     def __str__(self):
         """Returns the string value of the color object"""
-        return self.rgb if self.rgb else 'Unknown'
+        return f'{self.rgb}' if self.rgb else 'Unknown'
