8921699b3ecf1ea30c16d000fcd0c244a75b651c
buzonek <buzonekk@gmail.com>
2020-01-22 09:59:41 +0100

    submitting bite no 24

--- 44/test.py
+++ 44/test.py
@@ -5,7 +5,9 @@ alphabet = string.ascii_ + string.digits
 
 
 def gen_key(parts=4, chars_per_part=8):
-    return '-'.join([''.join([secrets.choice(alphabet) for i in range(chars_per_part)]) for j in range(parts)])
+    return '-'.join(
+        [''.join([secrets.choice(alphabet) for i in range(chars_per_part)]) for
+         j in range(parts)])
 
 
 print(gen_key(10, 10))
