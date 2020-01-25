4c277c710e5d71ae0d92327bb50e7d0a52211582
buzonek <buzonekk@gmail.com>
2020-01-25 15:40:16 +0100

    submitting bite no 68

---
+++ 68/clean.py
@@ -0,0 +1,6 @@
+from string import punctuation
+
+
+def remove_punctuation(input_string):
+    """Return a str with punctuation chars stripped out"""
+    return ''.join(x for x in input_string if x not in punctuation)
